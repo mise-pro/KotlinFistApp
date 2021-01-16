a1dcdddd2fdaa165c0fd52d672ffa4014ed51508
Михаил <40423854+mise-pro@users.noreply.github.com>
2021-01-16 21:02:34 +0300

    мок данных

---
+++ app/src/main/java/ru/promise/educationKtApp/model/Movie.kt
@@ -0,0 +1,12 @@
+package ru.promise.educationKtApp.model
+
+data class Movie(
+        val name: String,
+        val poster: String,
+        val pg: Int,
+        val tagline: String,
+        val rating: Int,
+        val reviews: Int,
+        val storyLine: String,
+        val actorList: List<Actor>
+)
